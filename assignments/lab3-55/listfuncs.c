0aa34a2bb7facd5ad4f18b76d740af2f31bf9736
coriandar <88608040+coriandar@users.noreply.github.com>
2023-08-07 20:49:09 +1200

    Add comments

--- assignments/lab3-55/listfuncs.c
+++ assignments/lab3-55/listfuncs.c
@@ -6,6 +6,17 @@
 #include <stdio.h>
 #include "prog4.h"
 
+/**
+ * Inserts a list item into a given list after the item
+ * with the specified number.
+ * 
+ * @param list_head - head of list.
+ * @param insertItem - item to be inserted.
+ * @param n - specified number, which item holds.
+ * 
+ * @return 1 - if insertion successful.
+ * @return 0 - if insertion unsuccessful, not successful if n not found.
+ **/
 int insert_after(List_item *list_head, List_item *insertItem, int n)
 {
     if (list_head == NULL)
@@ -15,10 +26,12 @@ int insert_after(List_item *list_head, List_item *insertItem, int n)
 
     if ((list_head->item_num != n) && (list_head->next != NULL))
     {
+        // traverse to next item
         return insert_after(list_head->next, insertItem, n);
     }
     else if (list_head->item_num == n)
     {
+        // inserts item between list_head and list_head->next
         insertItem->next = list_head->next;
         list_head->next = insertItem;
         return 1;
@@ -29,6 +42,15 @@ int insert_after(List_item *list_head, List_item *insertItem, int n)
     }
 }
 
+/**
+ * Removes a list item with the specified number from list.
+ * 
+ * @param list_head - head of list.
+ * @param n - specified number, which item holds.
+ * 
+ * @return 1 - if remove successful.
+ * @return 0 - if remove unsuccessful, not successful if n not found.
+ **/
 int remove_item(List_item *list_head, int n)
 {
     if (list_head == NULL)
@@ -48,17 +70,21 @@ int remove_item(List_item *list_head, int n)
     {
         if (list_head->next->item_num == n)
         {
+            // removes link to found item
             list_head->next = list_head->next->next;
             return 1;
         }
         else
         {
+            // traverse to next item
             return remove_item(list_head->next, n);
         }
     }
 }
 
-// Go through the list and print the numbers in the order of the list
+/**
+ * Go through the list and print the numbers in the order of the list
+ **/
 void print_linked_list(List_item *head)
 {
     if (head != NULL)
--- assignments/lab3-55/listfuncs.h
+++ assignments/lab3-55/listfuncs.h
@@ -6,6 +6,7 @@
 #ifndef LISTFUNCS_H
 #define LISTFUNCS_H
 
+// prototypes
 int insert_after(List_item*, List_item*, int);
 int remove_item(List_item*, int);
 void print_linked_list(List_item*);
